a1232571e7c1555e63d8b8aafc240989e5c47efd
vtortola <vtortola@gmail.com>
2014-07-29 00:38:18 +0100

    Basic commands

--- script/app.js
+++ script/app.js
@@ -6,9 +6,23 @@
         $scope.$apply();
     }, 100);
 
-    $scope.$on('console-input', function (e) {
+    $scope.$on('console-input', function (e, cinput) {
+        var command = cinput[0];
+        switch (command) {
+            case 'ver':
+            case 'version':
+                $scope.$broadcast('console-output', { output: true, text: ['Version 0.1Beta'], breakLine: true });
+                break;
 
-        $scope.$broadcast('console-output', { output: true, text: ['Welcome to vtortola.GitHub.io','lala'], breakLine: true });
+            case 'help':
+                $scope.$broadcast('console-output', { output: true, text: ['No commands are avaiable', 'This app is still in development:)'], breakLine: true });
+                break;
+
+            default:
+                $scope.$broadcast('console-output', { output: true, text: ['There is no such command'], breakLine: true });
+                break;
+        }
+        
     });
 });
 
